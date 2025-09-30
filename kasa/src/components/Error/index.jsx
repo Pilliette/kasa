4f103e51e4cfae69bf3092460f87e963a727cd7c
Pilliette <acrmbz@gmail.com>
2025-09-30 16:31:29 +0200

    Page 404 terminée

--- kasa/src/components/Error/index.jsx
+++ kasa/src/components/Error/index.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom"
+import styles from "./Error.module.sass"
 
 function Error() {
     return(
-        <div>
-            <span>🤦‍♀️ 404 🤦</span>
-            <p>Oups! La page que vous demandez n'existe pas.</p>
-            <Link to="/">Retourner sur la page d'accueil</Link>
+        <div className={ styles.error }>
+            <p className={ styles.errorCode }>404</p>
+            <p className={ styles.errorMessage }>Oups! La page que vous demandez n'existe pas.</p>
+            <Link className={ styles.errorReturnLink } to="/">Retourner sur la page d'accueil</Link>
         </div>
     )
 }
