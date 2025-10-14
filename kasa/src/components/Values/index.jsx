16fea21febfc3f64a391bcc328a6b56d7c4bb462
Pilliette <acrmbz@gmail.com>
2025-10-14 11:19:07 +0200

    Nettoyage du code

--- kasa/src/components/Values/index.jsx
+++ kasa/src/components/Values/index.jsx
@@ -5,16 +5,16 @@ import styles from "./Values.module.sass"
 function Values() {
     return(
         <div className={ styles.values }>
-            <Item title="Fiabilité" about>
+            <Item title="Fiabilité">
                 Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.
             </Item>
-            <Item title="Respect" about>
+            <Item title="Respect">
                 La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une expulsion de notre plateforme.
             </Item>
-            <Item title="Service" about>
+            <Item title="Service">
                 La qualité du service est au coeur de notre engagement chez Kasa. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance.
             </Item>
-            <Item title="Sécurité" about>
+            <Item title="Sécurité">
                 La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestiques pour nos hôtes.
             </Item>
         </div>
